c1b013cca5766e8949882d9b20e085ac78f9b8f8
Jack Davidson <jdavidson018@gmail.com>
2024-07-30 21:38:58 -0600

    feat: Added a new route for exploring more of svelte and tailwindcss

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+	console.log('Loaded Playground Page');
+</script>
+
+<div class="bg-stone-600">
+	<div class="p-2 lg:rounded-md lg:shadow-[-6px_8px_0.6px_0_#0000001A] bg-night-start lg:p-0">
+		<a href="/automated-bond-ladder" target="_blank"
+			><div class="text-white py-2 lg:flex lg:items-center lg:justify-between 2xl:gap-5">
+				<svg
+					class="hidden xl:block"
+					width="123"
+					height="63"
+					viewBox="0 0 123 63"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					><mask
+						id="e732970f08bddfa0aa9d04c5c7df05db"
+						maskUnits="userSpaceOnUse"
+						x="-3"
+						y="0"
+						width="126"
+						height="63"
+						style="mask-type: alpha;"
+						><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M-1.10338 0C-2.15085 0 -3 0.849146 -3 1.89662V6.94188C-3 7.98935 -2.15085 8.8385 -1.10338 8.8385H58.2062C59.2536 8.8385 60.1028 7.98935 60.1028 6.94188V1.89662C60.1028 0.849146 59.2536 0 58.2062 0H-1.10338ZM-1.10338 10.6885C-2.15085 10.6885 -3 11.5376 -3 12.5851V17.6304C-3 18.6778 -2.15085 19.527 -1.10338 19.527H70.7445C71.792 19.527 72.6411 18.6778 72.6411 17.6304V12.5851C72.6411 11.5376 71.792 10.6885 70.7445 10.6885H-1.10338ZM-3 23.479C-3 22.4315 -2.15085 21.5824 -1.10338 21.5824H83.2828C84.3303 21.5824 85.1795 22.4315 85.1795 23.479V28.5243C85.1795 29.5717 84.3303 30.4209 83.2828 30.4209H-1.10338C-2.15085 30.4209 -3 29.5717 -3 28.5243V23.479ZM-1.10338 32.2708C-2.15085 32.2708 -3 33.12 -3 34.1675V39.2127C-3 40.2602 -2.15085 41.1093 -1.10338 41.1093H96.0267C97.0742 41.1093 97.9233 40.2602 97.9233 39.2127V34.1675C97.9233 33.12 97.0742 32.2708 96.0267 32.2708H-1.10338ZM-3 45.0614C-3 44.0139 -2.15085 43.1648 -1.10338 43.1648H108.565C109.613 43.1648 110.462 44.0139 110.462 45.0614V50.1066C110.462 51.1541 109.613 52.0033 108.565 52.0033H-1.10338C-2.15086 52.0033 -3 51.1541 -3 50.1066V45.0614ZM-1.10338 53.8532C-2.15085 53.8532 -3 54.7024 -3 55.7498V60.7951C-3 61.8426 -2.15085 62.6917 -1.10338 62.6917H121.103C122.151 62.6917 123 61.8426 123 60.7951V55.7498C123 54.7024 122.151 53.8532 121.103 53.8532H-1.10338Z"
+							fill="#230B59"
+						></path></mask
+					><g mask="url(#e732970f08bddfa0aa9d04c5c7df05db)"
+						><rect
+							width="139.93"
+							height="68.2586"
+							transform="matrix(-1 0 0 1 136.93 -3.03687)"
+							fill="url(#8696c784675e2b3f9e43dd186eb9921f)"
+						></rect></g
+					><defs
+						><linearGradient
+							id="8696c784675e2b3f9e43dd186eb9921f"
+							x1="69.965"
+							y1="0"
+							x2="69.965"
+							y2="68.2586"
+							gradientUnits="userSpaceOnUse"
+							><stop stop-color="#C3CDF4"></stop><stop offset="1" stop-color="#FED0B9"
+							></stop></linearGradient
+						></defs
+					></svg
+				>
+				<div
+					class="flex flex-grow justify-between flex-col gap-0 2xl:flex-row items-center 2xl:gap-7"
+				>
+					<div class="flex flex-col md:flex-row md:gap-2 items-center">
+						<p
+							class="tracking-tight inline-block text-[30px] lg:text-[25px] 2xl:text-[30px] font-light leading-none md:leading-tight xl:leading-normal font-serif italic"
+						>
+							The First Decent Looking Piece Of design
+						</p>
+					</div>
+				</div>
+				<svg
+					class="hidden xl:block"
+					width="125"
+					height="63"
+					viewBox="0 0 125 63"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					><mask
+						id="39ce9e288ea8dd45a280fadb9ebdf489"
+						maskUnits="userSpaceOnUse"
+						x="0"
+						y="0"
+						width="126"
+						height="63"
+						style="mask-type: alpha;"
+						><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M124.103 62.6917C125.151 62.6917 126 61.8426 126 60.7951V55.7498C126 54.7024 125.151 53.8532 124.103 53.8532H64.7938C63.7464 53.8532 62.8972 54.7024 62.8972 55.7498V60.7951C62.8972 61.8426 63.7464 62.6917 64.7938 62.6917H124.103ZM124.103 52.0032C125.151 52.0032 126 51.1541 126 50.1066V45.0614C126 44.0139 125.151 43.1647 124.103 43.1647H52.2555C51.208 43.1647 50.3589 44.0139 50.3589 45.0614V50.1066C50.3589 51.1541 51.208 52.0032 52.2555 52.0032H124.103ZM126 39.2127C126 40.2602 125.151 41.1093 124.103 41.1093H39.7172C38.6697 41.1093 37.8205 40.2602 37.8205 39.2127V34.1674C37.8205 33.12 38.6697 32.2708 39.7172 32.2708H124.103C125.151 32.2708 126 33.12 126 34.1674V39.2127ZM124.103 30.4209C125.151 30.4209 126 29.5717 126 28.5242V23.479C126 22.4315 125.151 21.5824 124.103 21.5824H26.9733C25.9258 21.5824 25.0767 22.4315 25.0767 23.479V28.5242C25.0767 29.5717 25.9258 30.4209 26.9733 30.4209H124.103ZM126 17.6303C126 18.6778 125.151 19.5269 124.103 19.5269H14.435C13.3875 19.5269 12.5383 18.6778 12.5383 17.6303V12.5851C12.5383 11.5376 13.3875 10.6884 14.435 10.6884H124.103C125.151 10.6884 126 11.5376 126 12.5851V17.6303ZM124.103 8.83849C125.151 8.83849 126 7.98934 126 6.94186V1.89661C126 0.849133 125.151 -1.52588e-05 124.103 -1.52588e-05H1.89662C0.849144 -1.52588e-05 0 0.849133 0 1.89661V6.94186C0 7.98934 0.849144 8.83849 1.89662 8.83849H124.103Z"
+							fill="#230B59"
+						></path></mask
+					><g mask="url(#39ce9e288ea8dd45a280fadb9ebdf489)"
+						><rect
+							width="230"
+							height="69"
+							transform="matrix(1 0 0 -1 -14 66)"
+							fill="url(#7120632800dcefcdd73d3a614c9a2787)"
+						></rect></g
+					><defs
+						><linearGradient
+							id="7120632800dcefcdd73d3a614c9a2787"
+							x1="115"
+							y1="0"
+							x2="115"
+							y2="69"
+							gradientUnits="userSpaceOnUse"
+							><stop stop-color="#C3CDF4"></stop><stop offset="1" stop-color="#FED0B9"
+							></stop></linearGradient
+						></defs
+					></svg
+				>
+			</div></a
+		>
+	</div>
+</div>
