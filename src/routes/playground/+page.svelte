6c773f315b8eb3f30f4024bb6194513eb9321d57
Jack Davidson <jdavidson018@gmail.com>
2024-08-01 22:10:41 -0600

    feat: Moving slowly, demonstrating relative parents with absolute children

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -2,6 +2,13 @@
 	console.log('Loaded Playground Page');
 </script>
 
-<body class="relative antialiased tracking-tight text-gray-500 bg-black font-mono">
+<!-- <body class="relative antialiased tracking-tight text-gray-500 bg-black font-mono">
 	<div>Test</div>
-</body>
+</body> -->
+
+<div class="relative m-10 h-64 w-64 bg-black text-gray-500">
+	<p>Relative parent</p>
+	<div class="absolute bottom-0 left-0 ...">
+		<p>Absolute child</p>
+	</div>
+</div>
